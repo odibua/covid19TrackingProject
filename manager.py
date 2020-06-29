d2cb5734d5728c4111231bd61fcac77f10a88595
odibua <odibua@gmail.com>
2020-06-28 20:45:27 -0700

    Write skeleton for manager.py and add california state config

--- manager.py
+++ manager.py
@@ -2,6 +2,9 @@
 # Standard Python Imports
 # --------------------------
 import logging
+import os
+from os import path
+
 # --------------------------
 # Third Party Imports
 # --------------------------
@@ -10,9 +13,63 @@ import yaml as yaml
 # --------------------------
 # covid19Tracking Imports
 # --------------------------
+import utils
 
-logging.info("Open State Configuration file")
-state_config_file = open('states/states_config.yaml')
-state_conig = yaml.safe_load(state_config_file)
 
-logging.info(f"Extract relevant raw data for state, if it exists")
+def get_responses_from_config_files_in_dir(config_dir: str):
+    config_files = os.listdir(config_dir)
+    config_files = [config_file for config_file in config_files if config_file.endswith('.yaml')]
+    if len(config_files) > 0:
+        response_list, response_names, failed_response_names = utils.get_yaml_responses(config_file_list=config_files)
+    else:
+        response_list, response_names, failed_response_names = None, None, None
+
+    return response_list, response_names, failed_response_names
+
+
+logging.info("Open State Configuration file and get states to be processed")
+config_path = 'states/states_config.yaml'
+if not path.isfile(config_path):
+    raise ValueError(f"states_config.yaml not found in states directory")
+config_file = open(config_path)
+config = yaml.safe_load(config_file)
+state_list = config['STATES']
+
+logging.info(f"Get and process covid19 ethnicity data for each state and corresponding counties")
+for state in state_list:
+    state_name = state.lower()
+    logging.info(f"Processing {state_name}")
+    state_dirs = os.listdir(path.join('states', state_name))
+
+    state_config_path = path.join('states', state_name, 'configs')
+    logging.info("Get state level covid19 raw data with ethnicity")
+    state_response_list, state_data_type_names, failed_state_data_type_names = get_responses_from_config_files_in_dir(config_dir=state_config_path)
+    if state_response_list is None:
+        raise Warning(f"No state level config files exist for {state_name}")
+    else:
+        logging.info("Save state level raw covid 19 data with ethnicity")
+        raw_data_dir = path.join('states', state_name, 'raw_data')
+        if not path.isdir(raw_data_dir):
+            os.makedirs(raw_data_dir)
+        utils.save_raw_data(save_dir=raw_data_dir, response_list=state_response_list, data_type_names=state_data_type_names, failed_data_type_names=failed_state_data_type_names)
+
+    logging.info(f"Processing county level data for {state_name}")
+    state_county_dirs = os.listdir(path.join('states', state_name, 'counties'))
+    if len(state_county_dirs) > 0:
+        for state_county_dir in state_county_dirs:
+            logging.info(f"Getting and saving raw data for state: {state_name}, county: {state_county_dir}")
+            county_response_list, county_data_type_names, failed_county_data_type_names = get_responses_from_config_files_in_dir(config_dir=path.join('states', state_name, 'counties', state_county_dir))
+
+            if county_response_list is None:
+                raise Warning(f"No county level config files exist for {state_county_dir}")
+            else:
+                logging.info("Save county level raw covid 19 data with ethnicity")
+                raw_data_dir = path.join('states', state_name, 'counties', state_county_dir, 'raw_data')
+                if not path.isdir(raw_data_dir):
+                    os.makedirs(raw_data_dir)
+                utils.save_raw_data(save_dir=raw_data_dir, response_list=county_response_list,
+                                    data_type_names=county_data_type_names,
+                                    failed_data_type_names=failed_county_data_type_names)
+    else:
+        raise Warning(f"No county level data exists for {state_name}")
+
