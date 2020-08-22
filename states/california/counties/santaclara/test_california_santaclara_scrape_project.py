c80689df9e7b069446477e9776f32d85c10c2685
odibua <odibua@gmail.com>
2020-08-22 14:13:44 -0700

    Update circleci and tests to commit and push

--- states/california/counties/santaclara/test_california_santaclara_scrape_project.py
+++ states/california/counties/santaclara/test_california_santaclara_scrape_project.py
@@ -10,13 +10,15 @@ import unittest
 # --------------------------
 # covid19Tracking Imports
 # --------------------------
-from managers import scrape_manager
+from managers import add_commit_and_push, scrape_manager
 
 
 class TestCaliforniaSantaClaraScrapeAndProject(unittest.TestCase):
     def setUp(self):
         self.state_name = 'california'
         self.county_name = 'santaclara'
+        self.state_county_dir = f"states/{self.state_name}/counties/{self.county_name}/raw_data/"
 
     def test_scrape_manager(self):
         scrape_manager(state_name=self.state_name, county_name=self.county_name)
+        add_commit_and_push(state_county_dir=self.state_county_dir)
--- states/california/test_california_scrape_project.py
+++ states/california/test_california_scrape_project.py
@@ -10,12 +10,16 @@ import unittest
 # --------------------------
 # covid19Tracking Imports
 # --------------------------
-from managers import scrape_manager
+from managers import scrape_manager, add_commit_and_push
 
 
 class TestCaliforniaScrapeAndProject(unittest.TestCase):
     def setUp(self):
         self.state_name = 'california'
+        self.state_county_dir = 'states/california/raw_data/'
 
     def test_scrape_manager(self):
         scrape_manager(state_name=self.state_name)
+        add_commit_and_push(state_county_dir=self.state_county_dir)
+
+
