b867e405992564b6e71860cebc2733c2b34f6a8c
odibua <odibua@gmail.com>
2020-08-24 18:00:35 -0700

    Modify tests of projectors to use state and county arguments

--- states/california/counties/kern/test_california_kern_scrape_project.py
+++ states/california/counties/kern/test_california_kern_scrape_project.py
@@ -1,6 +1,7 @@
 # --------------------------
 # Standard Python Imports
 # --------------------------
+import pytest
 import unittest
 
 # --------------------------
@@ -10,18 +11,16 @@ import unittest
 # --------------------------
 # covid19Tracking Imports
 # --------------------------
-from managers import add_commit_and_push,scrape_manager, raw_to_ethnicity_case_csv_manager, raw_to_ethnicity_death_csv_manager
+from managers import raw_to_ethnicity_case_csv_manager
 
 
+@pytest.mark.usefixtures("project_bools")
 class TestCaliforniaKernScrapeAndProject(unittest.TestCase):
     def setUp(self):
         self.state_name = 'california'
         self.county_name = 'kern'
         self.state_county_dir = f"states/{self.state_name}/counties/{self.county_name}/raw_data/"
 
-    def test_scrape_manager(self):
-        scrape_manager(state_name=self.state_name, county_name=self.county_name)
-        add_commit_and_push(state_county_dir=self.state_county_dir)
-
     def test_raw_to_ethnicity_case_manager(self):
-        raw_to_ethnicity_case_csv_manager(state_name=self.state_name, county_name=self.county_name)
+        if self.project_case_bool:
+            raw_to_ethnicity_case_csv_manager(state_name=self.state_name)
