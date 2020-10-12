d6c02dded3320b8b221ec347f577843daefe8219
odibua <odibua@gmail.com>
2020-10-11 21:22:21 -0700

    Add gaussian curve fitting with radial basis function for time series data. Start using demographics to create reference data

--- analysis/analyze_timeseries.py
+++ analysis/analyze_timeseries.py
@@ -5,11 +5,13 @@ import argparse
 import collections
 import operator
 import os
-from typing import Dict, List, Tuple
+from typing import Callable, Dict, List, Tuple
 
 # --------------------------
 # Third Party Imports
 # --------------------------
+import importlib
+import numpy as np
 import pandas as pd
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
@@ -19,6 +21,7 @@ from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
 # covid19Tracking Imports
 # --------------------------
 from analysis.analyze_aggregate import open_csvs
+from utils import filter_projector_module, get_projector_module, get_class_in_projector_module
 
 
 def get_timeseries_counts_df_dict(df_dict: Dict[str, Dict[str, List[Tuple[str, pd.DataFrame]]]]
@@ -40,12 +43,60 @@ def get_timeseries_counts_df_dict(df_dict: Dict[str, Dict[str, List[Tuple[str, p
     return time_series_counts_df_dict
 
 
+def get_identifier_demographics_dict(state: str, county: str) -> Dict[str, float]:
+    if state != county:
+        state_county_dir = os.path.join('states', f'{state}', 'counties', f'{county}')
+    else:
+        state_county_dir = os.path.join('states', f'{state}')
+
+    state_county_dir_list = os.listdir(state_county_dir)
+    state_county_projector_list = filter_projector_module(projector_candidate_list=state_county_dir_list)
+    if len(state_county_projector_list) != 1:
+        raise ValueError(
+            f"ERROR: ONLY ONE PROJECTOR SHOULD BE IMPLEMENTED IN DIRECTORY. Found {len(state_county_projector_list)} for directory {state_county_dir}")
+
+    module_name = get_projector_module(state=state, county=county, projector_name=state_county_projector_list[0][0:-3])
+    state_county_projector_module = importlib.import_module(module_name)
+    projector_class = get_class_in_projector_module(module=state_county_projector_module, module_name=module_name)
+    return projector_class.ethnicity_demographics
+
+
+def fit_time_series_counts(df_dict: Dict[str, List[Tuple[str, pd.DataFrame]]], state: str) \
+        -> Dict[str, Dict[str, List[Tuple[str, GaussianProcessRegressor]]]]:
+    time_regression_dict = {}
+    for key in df_dict.keys():
+        if key not in time_regression_dict.keys():
+            time_regression_dict[key] = {}
+        for identifier_df_tuple in df_dict[key]:
+            identifier, df = identifier_df_tuple
+            identifier_demographics = get_identifier_demographics_dict(state=state, county=identifier)
+            import ipdb
+            ipdb.set_trace()
+            if identifier not in time_regression_dict[key].keys():
+                time_regression_dict[key][identifier] = []
+            for column in df.keys():
+                if column != 'time':
+                    num_bool = df[column].notnull()
+                    x_train, y_train = df['time'][num_bool].tolist(), df[column][num_bool].tolist()
+                    x_train, y_train = np.array(x_train).reshape((-1, 1)), np.array(y_train).reshape((-1, 1))
+
+                    kernel = C(1.0, (1e-3, 1e4)) * RBF(1.0, (1e-3, 1e4))
+                    gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=20, normalize_y=True)
+                    try:
+                        gp.fit(x_train, y_train)
+                        y_pred, sigma = gp.predict(x_train, return_std=True)
+                        nrmse = np.sqrt(np.mean((y_pred - np.array(y_train)) ** 2)) / np.mean(y_train)
+                        time_regression_dict[key][identifier].append((column, y_pred.ravel(), y_train.ravel(), gp.kernel_.k1.constant_value, gp.kernel_.k2.length_scale, nrmse))
+                    except:
+                        pass
+
+    return time_regression_dict
+
+
 def time_series_analysis(csv_df_dict: Dict[str, Dict[str, List[Tuple[str, pd.DataFrame]]]], state: str) -> None:
 
     time_series_counts_df_dict = get_timeseries_counts_df_dict(df_dict=csv_df_dict)
-    fit_reg_dict = fit_time_series_counts(df_dict=time_series_counts_df_dict)
-    import ipdb
-    ipdb.set_trace()
+    time_regression_dict = fit_time_series_counts(df_dict=time_series_counts_df_dict, state=state)
     pass
 
 
