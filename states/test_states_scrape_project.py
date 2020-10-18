9e217ceef3cdfa85bdff928e9f15c3de46620aee
Ohi Dibua <34965977+odibua@users.noreply.github.com>
2020-10-17 20:59:16 -0700

    Add code for scraping raw data from covid tracking project (#16)

---
+++ states/test_states_scrape_project.py
@@ -0,0 +1,36 @@
+# --------------------------
+# Standard Python Imports
+# --------------------------
+import pytest
+import unittest
+
+# --------------------------
+# Third Party Imports
+# --------------------------
+
+# --------------------------
+# covid19Tracking Imports
+# --------------------------
+from managers import scrape_manager, add_commit_and_push, case_parser_manager, death_parser_manager
+
+
+@pytest.mark.usefixtures("project_bools")
+class TestCaliforniaScrapeAndProject(unittest.TestCase):
+    def setUp(self):
+        self.state_name = 'states'
+        self.state_county_dir = 'states/raw_data/'
+
+    def test_scrape_manager(self):
+        if len(self.state_arg) == 0 or self.state_arg.lower() == self.state_name.lower():
+            scrape_manager(state_name=self.state_name)
+            add_commit_and_push(state_county_dir=self.state_county_dir)
+
+    def test_raw_to_ethnicity_case_manager(self):
+        if len(self.state_arg) == 0 or self.state_arg.lower() == self.state_name.lower():
+            if self.project_case_bool:
+                case_parser_manager(state_name=self.state_name)
+
+    def test_raw_to_ethnicity_death_manager(self):
+        if len(self.state_arg) == 0 or self.state_arg.lower() == self.state_name.lower():
+            if self.project_death_bool:
+                death_parser_manager(state_name=self.state_name)
